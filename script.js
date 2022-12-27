09bdfdc55ede6d64cd459978e7d5ac16cbdd5de3
mart3318 <12470793+mart3318@users.noreply.github.com>
2022-12-27 11:30:38 -0600

    Learning JS, first use of switch, case, and break

--- script.js
+++ script.js
@@ -34,4 +34,94 @@ let division = () => {
     document.getElementById("ResultDisplay").innerHTML = calculation
     }
 
-//print calculation
+    let input0 = () => {
+        input = 0
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+    let input1 = () => {
+        input = 1
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+    let input2 = () => {
+        input = 2
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+    let input3 = () => {
+        input = 3
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+    let input4 = () => {
+        input = 4
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+    
+    let input5 = () => {
+        input = 5
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+    let input6 = () => {
+        input = 6
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+    let input7 = () => {
+        input = 7
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+    let input8 = () => {
+        input = 8
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+    let input9 = () => {
+        input = 9
+        document.getElementById("ResultDisplay2").innerHTML = input
+    }
+
+
+    let operator
+
+    let inputAddition = () => {
+        operator = "+"
+    }
+
+    let inputSubtraction = () => {
+        operator = "-"
+    }
+
+    let inputMultiplication = () => {
+        operator = "*"
+    }
+
+    let inputDivision = () => {
+       operator = "/" 
+    }
+
+    let calculateResult = () => {
+        switch(operator) {
+            case "+":
+            console.log("Addition")
+            break
+            case "-":
+            console.log("Subtraction")
+            break
+            case "*":
+            console.log("Multiplication")
+            break
+            case "/":
+            console.log("Division")
+            break
+        }
+
+    }
+
+
+
+
+    //print calculation
